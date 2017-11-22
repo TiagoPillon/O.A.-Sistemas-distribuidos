d9ce1cf324a7d7ec90533e5ea1047e6ec6e795b0
TiagoPillon <tiagoplsilva@gmail.com>
2017-11-22 19:59:34 -0200

    Add files via upload

---
+++ Api-Rest/Models/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TesteWebAPI.Services.Models
+{
+    public class BalancoComercialPorPais
+    {
+        public int AnoBase { get; set; }
+        public string Pais { get; set; }
+        public string Sigla { get; set; }
+        public double ValorExportado { get; set; }
+        public double ValorImportado { get; set; }
+    }
+
+    public class BalancoComercialPorContinente
+    {
+        public int AnoBase { get; set; }
+        public string Continente { get; set; }
+        public double ValorExportado { get; set; }
+        public double ValorImportado { get; set; }
+    }
+}

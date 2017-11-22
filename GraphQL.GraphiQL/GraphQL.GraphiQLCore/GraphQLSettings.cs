79cd962eb0eba903ef01cd048401b9eb804133f3
TiagoPillon <tiagoplsilva@gmail.com>
2017-11-22 20:10:16 -0200

    Add files via upload

---
+++ GraphQL.GraphiQL/GraphQL.GraphiQLCore/GraphQLSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace GraphQL.GraphiQLCore
+{
+    public class GraphQLSettings
+    {
+        public PathString Path { get; set; } = "/api/graphql";
+        public Func<HttpContext, object> BuildUserContext { get; set; }
+    }
+}
